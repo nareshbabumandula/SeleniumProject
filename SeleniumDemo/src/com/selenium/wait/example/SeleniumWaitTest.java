a7cc64bb0a69f409e66dd70d42b356c7018881cb
Mandula Naresh Babu <naresh223@gmail.com>
2021-10-12 08:13:44 +0530

    12-Oct-2021:Naresh: Selenium wait methods and JavaScriptExecutor methods example

--- SeleniumDemo/src/com/selenium/wait/example/SeleniumWaitTest.java
+++ SeleniumDemo/src/com/selenium/wait/example/SeleniumWaitTest.java
@@ -1,5 +1,6 @@
 package com.selenium.wait.example;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -7,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -24,21 +27,22 @@ public class SeleniumWaitTest {
 
 	@Test
 	public void linkMethods() throws InterruptedException {
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // Implicit wait
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS); // Implicit wait
 		driver.get("https://www.mycontactform.com/"); // Load a new web page in the current browser window. 
 		driver.manage().window().maximize(); // Maximize the browser window
-		
 		// Explicit wait - WebDriverWait
 		WebDriverWait wait = new WebDriverWait(driver, 30);
-		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.partialLinkText("More Testimonials123"))));
-		//WebElement testimonials = driver.findElement(By.linkText(">> More Testimonials"));
-		WebElement testimonials = driver.findElement(By.partialLinkText("More Testimonials"));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.name("btnSubmit")));
+		driver.findElement(By.name("btnSubmit")).click();
+		//Fluent wait
+		FluentWait fwait = new FluentWait(driver);
+		fwait.withTimeout(10000, TimeUnit.MILLISECONDS);
+		fwait.pollingEvery(250, TimeUnit.MILLISECONDS);
+		fwait.ignoring(NoSuchElementException.class);
+		fwait.until(ExpectedConditions.elementToBeClickable(By.linkText("FREE ACCOUNT SIGN UP!")));
+		driver.findElement(By.linkText("FREE ACCOUNT SIGN UP!")).click();
 		
-		System.out.println(testimonials.getAttribute("href"));
-		System.out.println(testimonials.getTagName());
-		System.out.println(testimonials.getText());
-		testimonials.click();
-		
+		driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
 	}
 
 
